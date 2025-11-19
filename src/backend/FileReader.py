45dd20256cb36c4a07f01b4fa613d05e9c7c8aa9
JulianKonowalski <julian.konwalski@gmail.com>
2025-11-19 23:14:17 +0100

    remove newline characters and empty lines from parsed text

--- src/backend/FileReader.py
+++ src/backend/FileReader.py
@@ -1,11 +1,17 @@
+import os
 import io
 
 class FileReader:
 
     file: io.TextIOWrapper | None = None
+    filesize: int = 0
 
     def __init__(self, filepath: str) -> None:
         self.file = open(filepath, "r")
+        self.filesize = os.fstat(self.file.fileno()).st_size
 
     def readLine(self) -> str:
-        return self.file.readline()
+        data = ""
+        while data == "" and self.file.tell() < self.filesize:
+            data = self.file.readline().strip("\n")
+        return data
